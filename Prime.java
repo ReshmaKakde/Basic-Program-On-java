edf78ec33ea81af17087038899e411251bc006e4
Reshma kakde <128820179+ReshmaKakde@users.noreply.github.com>
2023-03-28 19:11:03 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,17 @@
+class Prime
+{
+public static void main(String [] args)
+{
+int no=7;
+int count=0;
+for(int i=1;i<=no;i++)
+{
+if(no%i==0)
+count++;
+}
+if(count==2)
+System.out.println("prime number");
+else
+System.out.println("Not a prime number");
+}
+}
